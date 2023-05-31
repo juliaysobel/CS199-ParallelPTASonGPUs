453162597aed0c78ea647890be192d601d933f85
juliaysobel <91011637+juliaysobel@users.noreply.github.com>
2023-05-31 16:56:29 +0800

    Motif length test for GPU

--- CS199-GPUParallelPTAS/parallel_rccmp_gpu.py
+++ CS199-GPUParallelPTAS/parallel_rccmp_gpu.py
@@ -242,7 +242,7 @@ if __name__ == "__main__":
     
     tic = time.time()
     
-    BENCHMARK_LENGTH = 1 # length of motif, number of residues
+    BENCHMARK_LENGTH = 2 # length of motif, number of residues
     r = 2 # sample size
     b = 2.5 # max ball size
     d = 0
