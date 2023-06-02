4cd6710b0c2f17f7959e2a4b84501f78f2af340b
juliaysobel <91011637+juliaysobel@users.noreply.github.com>
2023-06-02 18:28:05 +0800

    Ball size test CPU

--- CS199-GPUParallelPTAS/parallel_rccmp_cpu.py
+++ CS199-GPUParallelPTAS/parallel_rccmp_cpu.py
@@ -218,9 +218,9 @@ if __name__ == "__main__":
     
     tic = time.time()
     
-    BENCHMARK_LENGTH = 20
+    BENCHMARK_LENGTH = 15
     r = 2 # sample size
-    b = 17 # max ball size
+    b = 20 # max ball size
     d = 0
     min_trmsd = sys.maxsize
     svdtime = 0
