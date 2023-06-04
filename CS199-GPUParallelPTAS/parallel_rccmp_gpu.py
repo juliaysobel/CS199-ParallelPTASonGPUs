f3cbb531188d97423eb258d416e0494098bcdbf5
juliaysobel <91011637+juliaysobel@users.noreply.github.com>
2023-06-04 15:59:24 +0800

    Sample test for GPU

--- CS199-GPUParallelPTAS/parallel_rccmp_gpu.py
+++ CS199-GPUParallelPTAS/parallel_rccmp_gpu.py
@@ -243,9 +243,9 @@ if __name__ == "__main__":
     
     tic = time.time()
     
-    BENCHMARK_LENGTH = 7 # length of motif, number of residues
+    BENCHMARK_LENGTH = 3 # length of motif, number of residues
     r = 2 # sample size
-    b = 5 # max ball size
+    b = 2.5 # max ball size
     d = 0
     min_trmsd = sys.maxsize
     transfertime = []
