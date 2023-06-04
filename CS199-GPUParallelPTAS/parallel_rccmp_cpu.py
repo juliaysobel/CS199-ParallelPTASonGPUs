c4b9a0760425ed7264f6870c6f7c4c7f73bd0d1f
juliaysobel <91011637+juliaysobel@users.noreply.github.com>
2023-06-05 00:41:26 +0800

    Processor tests

--- CS199-GPUParallelPTAS/parallel_rccmp_cpu.py
+++ CS199-GPUParallelPTAS/parallel_rccmp_cpu.py
@@ -242,7 +242,7 @@ if __name__ == "__main__":
     print(len(sample_ranges))
 
     # parallelization step
-    num_processors = 19
+    num_processors = 60
     div = int(len(sample_ranges)/num_processors)
     pool = multiprocessing.Pool(num_processors)
     results = pool.map(impose_step,[[sample_ranges[i:i+div],centered_structs,min_trmsd,BENCHMARK_LENGTH,b,str(int(i/div))] for i in range(0,len(sample_ranges),div)])
